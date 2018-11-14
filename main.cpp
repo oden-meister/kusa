43a203f239921bbeb6a96afd82742a0784486bf1
GODofMEGANE <s18195@tokyo.kosen-ac.jp>
2018-11-14 17:20:31 +0900

    [add] 顔認証システムwwwwww

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <opencv2/opencv.hpp>
+#include <bits/stdc++.h>
+
+cv::Mat faceFind(cv::Mat &image,std::string &cascade_file){
+    cv::CascadeClassifier cascade;
+    cascade.load(cascade_file);
+
+    std::vector<cv::Rect> faces;
+    cascade.detectMultiScale(image, faces, 1.1,3,0,cv::Size(20,20));
+
+    for (int i = 0; i < faces.size(); i++){
+        rectangle(image, cv::Point(faces[i].x,faces[i].y),cv::Point(faces[i].x + faces[i].width,faces[i].y + faces[i].height),cv::Scalar(0,200,0),3,CV_AA);
+    }
+    return image;
+}
+
+int main(){
+    cv::Mat image = cv::imread("/home/megane/Downloads/sample.jpg",cv::IMREAD_COLOR);
+    std::string filename = "haarcascade_frontalface_default.xml";
+    cv::Mat detectFaceImage = faceFind(image, filename);
+    cv::imshow("detect face",detectFaceImage);
+    cv::waitKey(0);
+
+    return 0;
+}
