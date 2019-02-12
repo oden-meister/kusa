7355ee2e6c15d5fd182e1b843b4fd55f18a3379c
GODofMEGANE <s18195@tokyo.kosen-ac.jp>
2019-02-12 16:14:55 +0900

    顔の個数を出力 (おまけにimageが見つからないときの例外処理も書いといた)

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ cv::Mat faceFind(cv::Mat &image,std::string &cascade_file){
 
     std::vector<cv::Rect> faces;
     cascade.detectMultiScale(image, faces, 1.1,3,0,cv::Size(20,20));
+    std::cout << "Faces " << faces.size() << std::endl;
 
     for (int i = 0; i < faces.size(); i++){
         rectangle(image, cv::Point(faces[i].x,faces[i].y),cv::Point(faces[i].x + faces[i].width,faces[i].y + faces[i].height),cv::Scalar(0,200,0),3,CV_AA);
@@ -16,6 +17,10 @@ cv::Mat faceFind(cv::Mat &image,std::string &cascade_file){
 
 int main(){
     cv::Mat image = cv::imread("/home/megane/Downloads/sample.jpg",cv::IMREAD_COLOR);
+    if(image.empty()){
+        std::cout << "Image file isn't found." << std::endl;
+        return 0;
+    }
     std::string filename = "haarcascade_frontalface_default.xml";
     cv::Mat detectFaceImage = faceFind(image, filename);
     cv::imshow("detect face",detectFaceImage);
